c4c18c6da25b9a4672e22e4ead07eadd60ab4a03
Matthew Lemon <lemon@matthewlemon.com>
2017-07-03 21:15:07 +0100

    added date checker for journal file

--- pledgertools/pledger.py
+++ pledgertools/pledger.py
@@ -1,9 +1,12 @@
 import csv
+import re
 from typing import NamedTuple
 import datetime
 
+journal_date_regex = re.compile(r'^(?P<year>\d{4})?[\/\-\.]?(?P<month>\d{2})[\/\.\-](?P<day>\d{1,2})')
 
-class LedgerLine(NamedTuple):
+
+class BankCSVLine(NamedTuple):
     """Represents a single line from hledger journal file."""
     date: datetime.date
     description: str
@@ -23,11 +26,33 @@ def splat_date_str(d: str) -> datetime.date:
         int(d_list[0]))
 
 
-def parse(csv_file) -> list:
+def parse_csv(csv_file) -> list:
     with open(csv_file, 'r') as cf:
         csv_reader = csv.reader(cf)
-        return [LedgerLine(
-                    date=splat_date_str(line[0]),
-                    description=line[1],
-                    transaction_type=line[2],
-                    total=float(line[3])) for line in csv_reader]
+        return [BankCSVLine(
+            date=splat_date_str(line[0]),
+            description=line[1],
+            transaction_type=line[2],
+            total=float(line[3])) for line in csv_reader]
+
+
+def date_format_checker(date: str) -> bool:
+    m = re.match(journal_date_regex, date)
+    if m:
+        if int(m.group('month')) <= 12:
+            if m.group('month') in ['1', '01', '3', '03', '5', '05', '7', '07', '8', '08', '10', '12']:
+                if not int(m.group('day')) <= int('31'):
+                    return False
+            if m.group('month') in ['4', '04', '6', '06', '9', '09', '11']:
+                if not int(m.group('day')) <= int('30'):
+                    return False
+            if m.group('month') in ['2', '02']:
+                if m.group('day') not in ['28', '29']:
+                    return False
+        else:
+            return False
+    return True
+
+
+def parse_journal(journal_file) -> list:
+    pass
--- test/test_parser.py
+++ test/test_parser.py
@@ -1,4 +1,5 @@
 import os
+import re
 from tempfile import gettempdir
 from datetime import date
 
@@ -70,29 +71,37 @@ def incorrect_fourth_field() -> None:
     yield os.path.join(TMP_DIR, 'test_journal')
     os.unlink(os.path.join(TMP_DIR, 'test_journal'))
 
+@pytest.fixture
+def journal() -> None:
+    with open(os.path.join(TMP_DIR, 'test_ledger_journal'), 'w') as tf:
+        tf.write("""
+        2017/06/30 Zipadee Celery Ltd
+            expenses:gifts                      12.90
+            assets:hsbc current                 -12.90
+        """)
 
 def test_date(cleaned_csv_file) -> None:
-    parsed = pledger.parse(cleaned_csv_file)
+    parsed = pledger.parse_csv(cleaned_csv_file)
     assert parsed[0].date == date(2017, 6, 28)
     assert parsed[3].date == date(2017, 6, 27)
 
 
 def test_description(cleaned_csv_file) -> None:
-    parsed = pledger.parse(cleaned_csv_file)
+    parsed = pledger.parse_csv(cleaned_csv_file)
     assert parsed[0].description == "SAINSBURYS S/MKTS LONDON  SE12"
     assert parsed[1].description == "WWW.CALMAC.CO.UK INTERNET"
     assert parsed[2].description == "VODAFONE LTD"
 
 
 def test_transaction_type(cleaned_csv_file) -> None:
-    parsed = pledger.parse(cleaned_csv_file)
+    parsed = pledger.parse_csv(cleaned_csv_file)
     assert parsed[0].transaction_type == "VIS"
     assert parsed[1].transaction_type == "VIS"
     assert parsed[2].transaction_type == "DD"
 
 
 def test_cost_amount(cleaned_csv_file) -> None:
-    parsed = pledger.parse(cleaned_csv_file)
+    parsed = pledger.parse_csv(cleaned_csv_file)
     assert parsed[0].total == -6.20
     assert parsed[1].total == -49.00
     assert parsed[2].total == -26.35
@@ -101,19 +110,41 @@ def test_cost_amount(cleaned_csv_file) -> None:
 
 def test_first_field_is_not_date(incorrect_first_field) -> None:
     with pytest.raises(IndexError):
-        pledger.parse(incorrect_first_field)
+        pledger.parse_csv(incorrect_first_field)
 
 
 def test_second_field_is_wrong_type(incorrect_second_field) -> None:
     with pytest.raises(ValueError):
-        pledger.parse(incorrect_second_field)
+        pledger.parse_csv(incorrect_second_field)
 
 
 def test_third_field_is_wrong_type(incorrect_third_field) -> None:
     with pytest.raises(ValueError):
-        pledger.parse(incorrect_third_field)
+        pledger.parse_csv(incorrect_third_field)
 
 
 def test_fourth_field_is_wrong_type(incorrect_fourth_field) -> None:
     with pytest.raises(ValueError):
-        pledger.parse(incorrect_fourth_field)
+        pledger.parse_csv(incorrect_fourth_field)
+
+def test_journal_date_regex():
+    d_reg = re.match(pledger.journal_date_regex, '2016/06/30')
+    assert d_reg
+
+def test_bad_date():
+    assert not pledger.date_format_checker('2016/13/01')
+    assert not pledger.date_format_checker('2016/20/03')
+    assert not pledger.date_format_checker('2016/12/32')
+    assert not pledger.date_format_checker('2016/14/32')
+
+def test_good_date():
+    assert pledger.date_format_checker('2016/01/01')
+    assert pledger.date_format_checker('2016/02/29')
+
+def test_good_month_bad_day():
+    assert not pledger.date_format_checker('2016/02/30')
+    assert not pledger.date_format_checker('2017/04/31')
+
+#def test_date_field_from_journal_file(journal) -> None:
+#   parsed = pledger.parse_journal(journal)
+#   assert parsed[0].date == date(2017, 6, 30)
